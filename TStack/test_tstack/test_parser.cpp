0c66c7115bfbb9e93aafdc7e8c0b5d12a755dcb1
alex960714 <alex_9607@mail.ru>
2015-12-07 23:46:11 +0300

    Fixed some bugs

--- TStack/test_tstack/test_parser.cpp
+++ TStack/test_tstack/test_parser.cpp
@@ -12,9 +12,9 @@ TEST(TParser, can_create_parser_default)
 	ASSERT_NO_THROW(TParser p);
 }
 
-/*TEST(TParser, can_check_right_placement_of_parenthesis)
+TEST(TParser, can_check_right_placement_of_parenthesis)
 {
 	TParser p("4*(5+7)");
 
-	EXPECT_EQ(true, p.IsOper('+'));
-}*/
+	EXPECT_EQ(true, p.CheckParentheses());
+}
